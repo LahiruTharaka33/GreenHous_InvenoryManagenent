cba89e6409a34dea39fe56ff415eebf99fc6cd40
DSLTharaka <100894847+DSLTharaka@users.noreply.github.com>
2025-07-30 11:13:16 +0530

    new updated

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,12 @@
 import type { NextConfig } from "next";
-// @ts-ignore
-import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = withPWA({
+const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-});
+};
 
 export default nextConfig;
