3e2f89764e67cfdc3b9a448428f10337c598a952
DSLTharaka <100894847+DSLTharaka@users.noreply.github.com>
2025-07-30 10:54:32 +0530

    pwa didn't work

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,12 @@
 import type { NextConfig } from "next";
-// @ts-ignore
-import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = withPWA({
-  /* config options here */
+const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-    buildExcludes: [/middleware-manifest\.json$/],
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "offlineCache",
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
-});
+};
 
 export default nextConfig;
